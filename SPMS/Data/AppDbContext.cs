b78f714e424867a4dee79ea1d8fadf0447731497
ashishsharma-netdev <ashishsharma.netdev@gmail.com>
2026-09-04 13:38:06 +0530

    Add Student entity to database context

--- SPMS/Data/AppDbContext.cs
+++ SPMS/Data/AppDbContext.cs
@@ -1,26 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using SPMS.Models;
 
-namespace SPMS.Data
+namespace SPMS.Data;
+
+public class AppDbContext : DbContext
 {
- public class AppDbContext : DbContext
- {
- public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
- public DbSet<User> Users { get; set; }
- public DbSet<Vehicle> Vehicles { get; set; }
- public DbSet<ParkingSpace> ParkingSpaces { get; set; }
- public DbSet<ParkingSlot> ParkingSlots { get; set; }
- public DbSet<Booking> Bookings { get; set; }
- public DbSet<Pricing> Pricings { get; set; }
- public DbSet<Payment> Payments { get; set; }
- public DbSet<Caretaker> Caretakers { get; set; }
- public DbSet<ParkingLog> ParkingLogs { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<ParkingSpace> ParkingSpaces { get; set; }
+    public DbSet<ParkingSlot> ParkingSlots { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Pricing> Pricings { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Caretaker> Caretakers { get; set; }
+    public DbSet<ParkingLog> ParkingLogs { get; set; }
+    public DbSet<Student> Students { get; set; }
 
- protected override void OnModelCreating(ModelBuilder modelBuilder)
- {
- modelBuilder.Entity<ParkingSlot>().Property(p => p.RowVersion).IsRowVersion();
- base.OnModelCreating(modelBuilder);
- }
- }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ParkingSlot>().Property(p => p.RowVersion).IsRowVersion();
+        modelBuilder.Entity<Student>().HasIndex(s => s.Email).IsUnique();
+        base.OnModelCreating(modelBuilder);
+    }
 }
